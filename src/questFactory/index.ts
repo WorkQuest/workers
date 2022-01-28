d32f266cbaba7b2290666477b3a67c94f9668572
Microsxema <RobertGtr@protonmail.com>
2022-01-28 16:24:53 +0700

    fix names

--- src/questFactory/index.ts
+++ src/questFactory/index.ts
@@ -10,9 +10,9 @@ import { initDatabase, QuestFactoryBlockInfo, BlockchainNetworks } from '@workqu
 const abiFilePath = path.join(__dirname, '../../src/questFactory/abi/QuestFactory.json');
 const abi: any[] = JSON.parse(fs.readFileSync(abiFilePath).toString()).abi;
 
-const parseFactoryEventsFromHeight = configFactory.workQuestDevNetwork.questFactory.parseEventsFromHeight;
-const contractFactoryAddress = configFactory.workQuestDevNetwork.questFactory.contractAddress;
-const urlFactoryProvider = configFactory.workQuestDevNetwork.webSocketProvider;
+const parseEventsFromHeight = configFactory.workQuestDevNetwork.questFactory.parseEventsFromHeight;
+const contractAddress = configFactory.workQuestDevNetwork.questFactory.contractAddress;
+const wsLinkProvider = configFactory.workQuestDevNetwork.webSocketProvider;
 
 export async function init() {
   console.log('Start quest factory listener');
@@ -20,7 +20,7 @@ export async function init() {
   await initDatabase(configDatabase.dbLink, false, true);
 
   const web3Factory = new Web3(
-    new Web3.providers.WebsocketProvider(urlFactoryProvider, {
+    new Web3.providers.WebsocketProvider(wsLinkProvider, {
       clientConfig: {
         keepalive: true,
         keepaliveInterval: 60000, // ms
@@ -37,17 +37,17 @@ export async function init() {
     where: { network: BlockchainNetworks.workQuestDevNetwork },
     defaults: {
       network: BlockchainNetworks.workQuestDevNetwork,
-      lastParsedBlock: parseFactoryEventsFromHeight,
+      lastParsedBlock: parseEventsFromHeight,
     },
   });
 
-  if (questFactoryInfo.lastParsedBlock < parseFactoryEventsFromHeight) {
-    questFactoryInfo.lastParsedBlock = parseFactoryEventsFromHeight;
+  if (questFactoryInfo.lastParsedBlock < parseEventsFromHeight) {
+    questFactoryInfo.lastParsedBlock = parseEventsFromHeight;
 
     await questFactoryInfo.save();
   }
 
-  const questFactoryContract = new web3Factory.eth.Contract(abi, contractFactoryAddress);
+  const questFactoryContract = new web3Factory.eth.Contract(abi, contractAddress);
   const questFactoryProvider = new QuestProvider(web3Factory, questFactoryContract);
   const questFactoryController = new QuestFactoryController(questFactoryProvider, BlockchainNetworks.workQuestDevNetwork);
 
