33030439896b39dd1a8daa7ee69839faeb4dce4b
Ilya Kasimov <adamoffilya@mail.ru>
2022-03-15 12:51:30 +0700

    [update] fix deploy settings

--- src/wqt/config/config.wqt.ts
+++ src/wqt/config/config.wqt.ts
@@ -5,10 +5,10 @@ config({ path: __dirname + '/../../../.env.wqt' });
 export default {
   network: process.env.BLOCKCHAIN_NETWORK, // workQuestDevNetwork, workQuestTestNetwork, workQuestMainNetwork
   workQuestDevNetwork: {
-    contractAddress: process.env.WQT_WQ_DEV_NETWORK_CONTRACT,
+    contractAddress: process.env.WORK_QUEST_DEV_NETWORK_WQT_CONTRACT,
     linkTendermintProvider: process.env.WORK_QUEST_DEV_NETWORK_TENDERMINT_PROVIDER,
     linkRpcProvider: process.env.WORK_QUEST_DEV_NETWORK_RPC_PROVIDER,
-    parseEventsFromHeight: parseInt(process.env.WQT_WQ_DEV_NETWORK_PARSE_EVENTS_FROM_HEIGHT),
+    parseEventsFromHeight: parseInt(process.env.WORK_QUEST_DEV_NETWORK_WQT_PARSE_EVENTS_FROM_HEIGHT),
   },
   workQuestTestNetwork: {
   },
