6207d2ffa35c75dbcda4c65cdacb062b158b8e4b
Microsxema <RobertGtr@protonmail.com>
2022-04-07 14:30:59 +0700

    [update]

--- src/quest/src/controllers/models/QuestModelController.ts
+++ src/quest/src/controllers/models/QuestModelController.ts
@@ -18,7 +18,7 @@ export class QuestModelController {
   }
 
   public startQuest(): Promise<any> {
-    return this.quest.update({ status: QuestStatus.ExecutionOfWork });
+    return this.quest.update({ status: QuestStatus.ExecutionOfWork, startedAt: Date.now() });
   }
 
   public finishWork(): Promise<any> {
