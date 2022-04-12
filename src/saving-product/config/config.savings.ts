0d9af5d11cc024b2eab262c8d5b8c69b77f97d13
hojttaler <adamoffilya@mail.ru>
2022-04-12 12:23:39 +0700

    [update] SavingProduct: Update config env names

--- src/saving-product/config/config.savings.ts
+++ src/saving-product/config/config.savings.ts
@@ -6,9 +6,9 @@ export default {
   logLevel: 'debug',
   network: process.env.BLOCKCHAIN_NETWORK, // workQuestDevNetwork, workQuestTestNetwork, workQuestMainNetwork
   workQuestDevNetwork: {
-    contractAddress: process.env.WORK_QUEST_DEV_NETWORK_SAVINGS_CONTRACT_ADDRESS,
+    contractAddress: process.env.WORK_QUEST_DEV_NETWORK_SAVING_PRODUCT_CONTRACT_ADDRESS,
     linkRpcProvider: process.env.WORK_QUEST_DEV_NETWORK_RPC_PROVIDER,
-    parseEventsFromHeight: parseInt(process.env.WORK_QUEST_DEV_NETWORK_SAVINGS_PARSE_EVENTS_FROM_HEIGHT),
+    parseEventsFromHeight: parseInt(process.env.WORK_QUEST_DEV_NETWORK_SAVING_PRODUCT_PARSE_EVENTS_FROM_HEIGHT),
   },
   workQuestTestNetwork: {
   },
