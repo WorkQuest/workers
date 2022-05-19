bd4b2f62f66ad18b0df4566a4759b60cbde02c2d
Alexander Bokovoy <robzombieg@gmail.com>
2022-05-19 14:11:25 +0700

    [feature] fixes swapWqt by comments v1

--- src/swapUsdt/jobs/sendFirstWqt.ts
+++ src/swapUsdt/jobs/sendFirstWqt.ts
@@ -27,7 +27,7 @@ export default async function (payload: SendFirstWqtPayload) {
         status: swapUsdtStatus.SwapCreated
       }
     }
-  )
+  );
 
   if (!sendWqt) {
     Logger.warn('Can`t find Swap initialized event handler: event "%s" (tx hash "%s") is skipped because the payment happened earlier',
@@ -51,7 +51,7 @@ export default async function (payload: SendFirstWqtPayload) {
 
   const nonce = await web3.eth.getTransactionCount(faucetWallet.address);
 
-  const amountValue = new BigNumber(payload.amount).shiftedBy(+18).toFormat({ decimalSeparator: '' }).toString()
+  const amountValue = new BigNumber(payload.amount).shiftedBy(+18).toFormat({ decimalSeparator: '' }).toString();
 
   const txObject = {
     from: faucetWallet.address,
@@ -59,24 +59,25 @@ export default async function (payload: SendFirstWqtPayload) {
     to: payload.recipientWallet,
     value: amountValue,
     nonce: nonce
-  }
-  const gasLimit = await web3.eth.estimateGas(txObject)
+  };
+
+  const gasLimit = await web3.eth.estimateGas(txObject);
 
   // @ts-ignore
-  const txFee = (amountValue - (Number(gasPrice) * gasLimit)) - ((amountValue * payload.ratio) / 100)
+  const txFee = (amountValue - (Number(gasPrice) * gasLimit)) - ((amountValue * payload.ratio) / 100);
 
-  const sendWqtAmount = new BigNumber(txFee).toFormat({ decimalSeparator: '' }).toString()
+  const sendWqtAmount = new BigNumber(txFee).toFormat({ decimalSeparator: '' }).toString();
 
-  txObject['gas'] = gasLimit
-  txObject.value = sendWqtAmount
+  txObject['gas'] = gasLimit;
+  txObject.value = sendWqtAmount;
 
   const sendTrans = await web3.eth.sendTransaction(txObject, async (error, hash) => {
     if (error) {
       await sendWqt.update({
         status: swapUsdtStatus.SwapError
-      })
+      });
     }
-  })
+  });
 
   await sendWqt.update({
     transactionHash: sendTrans.transactionHash,
@@ -85,5 +86,5 @@ export default async function (payload: SendFirstWqtPayload) {
     amount: sendWqtAmount,
     status: swapUsdtStatus.SwapCreated,
     gasUsed: sendTrans.gasUsed
-  })
-}
+  });
+};
